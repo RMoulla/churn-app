c03d83fe83b7e069b6620e00391670cd766cfc7f
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2024-09-10 02:39:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,16 +6,19 @@ import joblib
 # Chargement des données
 data = pd.read_csv('data/customer_churn.csv')
 
-# Préparation des features et target
-X = data.drop('Churn', axis=1)
-y = data['Churn']
+# Filtrer les 5 variables numériques et la variable cible 'Churn'
+numeric_data = data[['Age', 'Total_Purchase', 'Account_Manager', 'Years', 'Num_Sites', 'Churn']]
 
-# Division en données d'entraînement et de test
+# Préparation des features (X) et de la cible (y)
+X = numeric_data.drop('Churn', axis=1)
+y = numeric_data['Churn']
+
+# Division des données en données d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entraînement du modèle
+# Entraînement du modèle RandomForest
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Sauvegarde du modèle
+# Sauvegarde du modèle dans un fichier .pkl
 joblib.dump(model, 'churn-model.pkl')
