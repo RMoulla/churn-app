3336b168940c4aaac3f2b6c596c383cbab5d43d1
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2024-11-28 20:41:59 +0100

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -5,20 +5,20 @@ from sklearn.linear_model import LogisticRegression
 
 def test_train_model_file_exists():
     """Vérifie que le fichier churn_model_clean.pkl est créé après exécution de train.py"""
-    assert os.path.exists('data/churn_model_clean.pkl'), (
+    assert os.path.exists('churn_model.pkl'), (
         "Le fichier churn_model_clean.pkl n'existe pas après l'exécution de train.py."
     )
 
 def test_train_model_loading():
     """Vérifie que le fichier sauvegardé contient un modèle LogisticRegression"""
-    model = joblib.load('data/churn_model_clean.pkl')
+    model = joblib.load('churn_model.pkl')
     assert isinstance(model, LogisticRegression), (
         "Le fichier churn_model_clean.pkl ne contient pas un modèle LogisticRegression."
     )
 
 def test_train_model_prediction():
     """Vérifie que le modèle entraîné peut prédire sur un sous-ensemble des données"""
-    model = joblib.load('data/churn_model_clean.pkl')
+    model = joblib.load('churn_model.pkl')
     data = pd.read_csv('data/customer_churn.csv')
     X = data[['Age', 'Account_Manager', 'Years', 'Num_Sites']]
 
