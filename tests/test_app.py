1eeebeab5b3fa30f42f5a7b7c1b55a6804d17d17
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2024-11-28 20:59:03 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,13 +17,18 @@ def test_predict_route_valid(client):
     """Vérifie que la route /predict retourne une prédiction pour des données valides"""
     data = {
         'Age': 45,
+        'Total_Purchase': 5000.0,
         'Account_Manager': 1,
         'Years': 5,
         'Num_Sites': 3
     }
+    # Envoyer les données en tant que formulaire
     response = client.post('/predict', data=data)
     assert response.status_code == 200, "La route /predict ne retourne pas un statut 200."
-    assert 'churn_prediction' in response.get_json(), "La réponse de /predict ne contient pas 'churn_prediction'."
+
+    # Vérifiez que la réponse contient une clé 'churn_prediction'
+    json_data = response.get_json()
+    assert 'churn_prediction' in json_data, "La réponse ne contient pas de clé 'churn_prediction'."
 
 def test_predict_route_invalid(client):
     """Vérifie que la route /predict retourne une erreur pour des données invalides"""
